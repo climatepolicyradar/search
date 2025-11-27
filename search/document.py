1048ee510b095aa7ac42939a7511b5fdbe50f6b5
harrisonpim <hello@harrisonpim.com>
2025-11-27 13:47:58 +0000

    add original_passage_id to Passages and original_document_id to Documents

--- search/document.py
+++ search/document.py
@@ -1,5 +1,5 @@
 from knowledge_graph.identifiers import Identifier
-from pydantic import AnyHttpUrl, BaseModel, computed_field
+from pydantic import AnyHttpUrl, BaseModel, Field, computed_field
 
 from search.label import Label
 
@@ -7,13 +7,24 @@ from search.label import Label
 class Document(BaseModel):
     """Base class for a CPR document"""
 
-    title: str
-    source_url: AnyHttpUrl
-    description: str
-    labels: list[Label] = []
+    title: str = Field(description="The title of the document")
+    source_url: AnyHttpUrl = Field(
+        description="The URL where the original document can be found"
+    )
+    description: str = Field(description="A description or summary of the document")
+    original_document_id: str = Field(
+        description=(
+            "The original ID of the document from the source system, "
+            "e.g. a document's ID in the main CPR apps"
+        )
+    )
+    labels: list[Label] = Field(
+        default=[],
+        description="List of which are associated with this document, eg metadata like geography, sector, etc.",
+    )
 
     @computed_field
     @property
     def id(self) -> Identifier:
-        """Return a unique ID for the document"""
+        """A canonical identifier for the document"""
         return Identifier.generate(self.title, self.source_url)
--- search/passage.py
+++ search/passage.py
@@ -1,5 +1,5 @@
 from knowledge_graph.identifiers import Identifier
-from pydantic import BaseModel, computed_field
+from pydantic import BaseModel, Field, computed_field
 
 from search.label import Label
 
@@ -7,12 +7,25 @@ from search.label import Label
 class Passage(BaseModel):
     """Base class for a passage"""
 
-    text: str
-    document_id: Identifier
-    labels: list[Label]
+    text: str = Field(description="The text content of the passage")
+    document_id: Identifier = Field(
+        description="Canonical ID for the document this passage belongs to"
+    )
+    labels: list[Label] = Field(
+        description=(
+            "List of which are associated with this passage, "
+            "eg topics identified by our classifiers"
+        ),
+    )
+    original_passage_id: str = Field(
+        description=(
+            "The original ID of the passage from the source system, "
+            "e.g. a passage's ID in the main CPR apps"
+        )
+    )
 
     @computed_field
     @property
     def id(self) -> Identifier:
-        """Return a unique ID for the passage"""
+        """A canonical identifier for the passage"""
         return Identifier.generate(self.text, self.document_id)
