798626267fe65804b6656f4ad811b3c99ecd4d15
harrisonpim <hello@harrisonpim.com>
2025-12-01 15:46:05 +0000

    specify the return type on get_XYZ_search_engine

--- api/__init__.py
+++ api/__init__.py
@@ -4,6 +4,7 @@ import logging
 
 from search.aws import download_file_from_s3, get_bucket_name
 from search.config import DATA_DIR
+from search.engines import DocumentSearchEngine, LabelSearchEngine, PassageSearchEngine
 from search.engines.duckdb import (
     DuckDBDocumentSearchEngine,
     DuckDBLabelSearchEngine,
@@ -37,19 +38,19 @@ async def download_required_datasets_from_s3():
             raise
 
 
-def get_label_search_engine():
+def get_label_search_engine() -> LabelSearchEngine:
     """Get the label search engine instance."""
     engine_class, filename = PREFERRED_ENGINES["labels"]
     return engine_class(str(DATA_DIR / filename))
 
 
-def get_passage_search_engine():
+def get_passage_search_engine() -> PassageSearchEngine:
     """Get the passage search engine instance."""
     engine_class, filename = PREFERRED_ENGINES["passages"]
     return engine_class(str(DATA_DIR / filename))
 
 
-def get_document_search_engine():
+def get_document_search_engine() -> DocumentSearchEngine:
     """Get the document search engine instance."""
     engine_class, filename = PREFERRED_ENGINES["documents"]
     return engine_class(str(DATA_DIR / filename))
