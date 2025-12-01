592f3ceb5bdd031f119af5f4ddcc19ba94a1eb8d
harrisonpim <hello@harrisonpim.com>
2025-12-01 14:27:57 +0000

    proper type inheritance for the json search engine

--- search/engines/json.py
+++ search/engines/json.py
@@ -1,4 +1,4 @@
-from typing import Any, Sequence, TypeVar
+from typing import Any, Generic, Sequence, TypeVar
 
 from knowledge_graph.identifiers import Identifier
 from pydantic import BaseModel
@@ -13,7 +13,7 @@ from search.engines import (
 from search.label import Label
 from search.passage import Passage
 
-TModel = TypeVar("TModel", bound=BaseModel)
+TModel = TypeVar("TModel", Label, Passage, Document)
 
 
 def serialise_pydantic_list_as_jsonl[T: BaseModel](models: Sequence[T]) -> str:
@@ -43,10 +43,15 @@ def deserialise_pydantic_list_from_jsonl[T: BaseModel](
     return models
 
 
-class JSONSearchEngine(SearchEngine):
-    """A search engine that searches for primitives in a JSONL file."""
+class JSONSearchEngine(SearchEngine, Generic[TModel]):
+    """
+    A search engine that searches for primitives in a JSONL file.
 
-    model_class: type[BaseModel] | None = None
+    TModel must be one of the Primitive types: Label, Passage, or Document.
+    """
+
+    model_class: type[TModel] | None = None
+    items: list[TModel]
 
     def __init__(self, file_path: str, model_class: type[TModel] | None = None):
         """Initialize the JSON search engine."""
@@ -82,7 +87,7 @@ class JSONSearchEngine(SearchEngine):
         return split_token.join(str(component) for component in components).lower()
 
 
-class JSONLabelSearchEngine(JSONSearchEngine, LabelSearchEngine):
+class JSONLabelSearchEngine(JSONSearchEngine[Label], LabelSearchEngine):
     """A search engine that searches for labels in a JSONL file."""
 
     model_class = Label
@@ -112,7 +117,7 @@ class JSONLabelSearchEngine(JSONSearchEngine, LabelSearchEngine):
         ]
 
 
-class JSONPassageSearchEngine(JSONSearchEngine, PassageSearchEngine):
+class JSONPassageSearchEngine(JSONSearchEngine[Passage], PassageSearchEngine):
     """A search engine that searches for passages in a JSONL file."""
 
     model_class = Passage
@@ -127,7 +132,7 @@ class JSONPassageSearchEngine(JSONSearchEngine, PassageSearchEngine):
         ]
 
 
-class JSONDocumentSearchEngine(JSONSearchEngine, DocumentSearchEngine):
+class JSONDocumentSearchEngine(JSONSearchEngine[Document], DocumentSearchEngine):
     """A search engine that searches for documents in a JSONL file."""
 
     model_class = Document
