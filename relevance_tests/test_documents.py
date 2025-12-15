00cfead36c10e9c442ca6ac591527ed9e0ee238d
juliesaigusaCPR <julie.saigusa@climatepolicyradar.org>
2025-12-15 16:35:33 +0000

    Sci 651 passage test runner (#15)
    * add passage test runner
    
    * add passage test runner to just test-relevance
    
    * Sci 651 document test runner (#16)
    
    * add test_documents to just test-relevance
    
    * add document test runner and more detailed logging
    
    * address comment: remove redundant method

--- relevance_tests/test_documents.py
+++ relevance_tests/test_documents.py
@@ -1,49 +1,50 @@
 from relevance_tests import TestResult, generate_test_run_id, save_test_results_as_jsonl
-from search.config import LABELS_PATH_STEM, TEST_RESULTS_DIR
-from search.engines.duckdb import DuckDBLabelSearchEngine
-from search.label import Label
+from search.config import DOCUMENTS_PATH_STEM, TEST_RESULTS_DIR
+from search.document import Document
+from search.engines.duckdb import DuckDBDocumentSearchEngine
 from search.logging import get_logger
 from search.testcase import FieldCharacteristicsTestCase, RecallTestCase
 
-LabelTestResult = TestResult[Label]
+DocumentTestResult = TestResult[Document]
 
 
 logger = get_logger(__name__)
 
-engines = [DuckDBLabelSearchEngine(db_path=LABELS_PATH_STEM.with_suffix(".duckdb"))]
+engines = [
+    DuckDBDocumentSearchEngine(db_path=DOCUMENTS_PATH_STEM.with_suffix(".duckdb"))
+]
 
-
+# TODO: Add proper test cases for documents
 test_cases = [
     RecallTestCase(
         search_terms="flood",
         expected_result_ids=["pdhcqueu"],
-        description="search should find labels related to flood",
+        description="search should find documents related to flood",
     ),
     FieldCharacteristicsTestCase(
         search_terms="nz",
-        characteristics_test=lambda label: (
-            "new zealand" in label.preferred_label.lower()
-        )
-        or ("net zero" in label.preferred_label.lower()),  # type: ignore
+        characteristics_test=lambda document: ("new zealand" in document.title.lower())
+        or ("net zero" in document.title.lower()),  # type: ignore
         all_or_any="all",
-        description="search for nz should return either new zealand or net zero",
+        description="search for nz should return either new zealand or net zero in the document title",
     ),
 ]
 
 
-def test_labels():
-    """Test labels"""
+def test_documents():
+    """Test documents"""
 
     for engine in engines:
-        engine_test_results: list[LabelTestResult] = []
-        logger.info(f"Testing label test cases against {engine.name}")
+        engine_test_results: list[DocumentTestResult] = []
+        logger.info(f"Testing document test cases against {engine.name}")
+
         for test_case in test_cases:
             logger.info(
                 f"Running test case: {test_case.name}: {test_case.search_terms}"
             )
             test_passed, search_results = test_case.run_against(engine)
 
-            test_result = LabelTestResult(
+            test_result = DocumentTestResult(
                 test_case=test_case,
                 passed=test_passed,
                 search_engine_id=engine.id,
@@ -53,11 +54,11 @@ def test_labels():
 
         test_run_id = generate_test_run_id(engine, test_cases, engine_test_results)
         output_file_path = (
-            TEST_RESULTS_DIR / "labels" / f"{engine.name}_{test_run_id}.jsonl"
+            TEST_RESULTS_DIR / "documents" / f"{engine.name}_{test_run_id}.jsonl"
         )
 
         save_test_results_as_jsonl(engine_test_results, output_file_path)
 
 
 if __name__ == "__main__":
-    test_labels()
+    test_documents()
--- relevance_tests/test_passages.py
+++ relevance_tests/test_passages.py
@@ -1,49 +1,47 @@
 from relevance_tests import TestResult, generate_test_run_id, save_test_results_as_jsonl
-from search.config import LABELS_PATH_STEM, TEST_RESULTS_DIR
-from search.engines.duckdb import DuckDBLabelSearchEngine
-from search.label import Label
+from search.config import PASSAGES_PATH_STEM, TEST_RESULTS_DIR
+from search.engines.duckdb import DuckDBPassageSearchEngine
 from search.logging import get_logger
+from search.passage import Passage
 from search.testcase import FieldCharacteristicsTestCase, RecallTestCase
 
-LabelTestResult = TestResult[Label]
+PassageTestResult = TestResult[Passage]
 
 
 logger = get_logger(__name__)
 
-engines = [DuckDBLabelSearchEngine(db_path=LABELS_PATH_STEM.with_suffix(".duckdb"))]
+engines = [DuckDBPassageSearchEngine(db_path=PASSAGES_PATH_STEM.with_suffix(".duckdb"))]
 
-
+# TODO: Add proper test cases for passages
 test_cases = [
     RecallTestCase(
         search_terms="flood",
         expected_result_ids=["pdhcqueu"],
-        description="search should find labels related to flood",
+        description="search should find passages related to flood",
     ),
     FieldCharacteristicsTestCase(
         search_terms="nz",
-        characteristics_test=lambda label: (
-            "new zealand" in label.preferred_label.lower()
-        )
-        or ("net zero" in label.preferred_label.lower()),  # type: ignore
+        characteristics_test=lambda passage: ("new zealand" in passage.text.lower())
+        or ("net zero" in passage.text.lower()),  # type: ignore
         all_or_any="all",
-        description="search for nz should return either new zealand or net zero",
+        description="search for nz should return either new zealand or net zero in the passage text",
     ),
 ]
 
 
-def test_labels():
-    """Test labels"""
+def test_passages():
+    """Test passages"""
 
     for engine in engines:
-        engine_test_results: list[LabelTestResult] = []
-        logger.info(f"Testing label test cases against {engine.name}")
+        engine_test_results: list[PassageTestResult] = []
+        logger.info(f"Testing passage test cases against {engine.name}")
         for test_case in test_cases:
             logger.info(
                 f"Running test case: {test_case.name}: {test_case.search_terms}"
             )
             test_passed, search_results = test_case.run_against(engine)
 
-            test_result = LabelTestResult(
+            test_result = PassageTestResult(
                 test_case=test_case,
                 passed=test_passed,
                 search_engine_id=engine.id,
@@ -53,11 +51,11 @@ def test_labels():
 
         test_run_id = generate_test_run_id(engine, test_cases, engine_test_results)
         output_file_path = (
-            TEST_RESULTS_DIR / "labels" / f"{engine.name}_{test_run_id}.jsonl"
+            TEST_RESULTS_DIR / "passages" / f"{engine.name}_{test_run_id}.jsonl"
         )
 
         save_test_results_as_jsonl(engine_test_results, output_file_path)
 
 
 if __name__ == "__main__":
-    test_labels()
+    test_passages()
