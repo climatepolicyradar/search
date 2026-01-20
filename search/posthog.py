2a7ceae43fc017a0ad1c363f6d9e4dee8f9c284f
juliesaigusaCPR <julie.saigusa@climatepolicyradar.org>
2026-01-20 16:52:45 +0000

    add searcher calculation (#48)
    * add searcher calculation
    
    * validate input dates and filter to prod domains
    
    * address review comment

--- search/posthog.py
+++ search/posthog.py
@@ -1,11 +1,19 @@
 """Helpers for interacting with PostHog"""
 
+from datetime import date
 from typing import Any
 
 import requests
-from pydantic import NonNegativeInt
+from pydantic import (
+    BaseModel,
+    NonNegativeFloat,
+    NonNegativeInt,
+    ValidationError,
+    model_validator,
+)
 
 from search.config import (
+    POSTHOG_CPR_DOMAINS,
     POSTHOG_HOST,
     POSTHOG_PARAM_NAME,
     POSTHOG_PROJECT_ID,
@@ -20,6 +28,24 @@ class Count(NonNegativeInt):
     """A count of a value returned from PostHog"""
 
 
+class Percentage(NonNegativeFloat):
+    """A percentage value returned from PostHog"""
+
+
+class DateRange(BaseModel):
+    """An inclusive range of dates for a HogQL query"""
+
+    date_from: date
+    date_to: date
+
+    @model_validator(mode="after")
+    def check_date_order(self):
+        """Check if the date range is valid"""
+        if self.date_from > self.date_to:
+            raise ValueError("Date from must be before date to")
+        return self
+
+
 class PostHogSession:
     """Session for querying PostHog analytics data."""
 
@@ -29,6 +55,18 @@ class PostHogSession:
         )
         self.host = POSTHOG_HOST
         self.project_id = POSTHOG_PROJECT_ID
+        # to filter out internal users mainly, in HogQL queries
+        self.cpr_domains = "(" + ", ".join(f"'{d}'" for d in POSTHOG_CPR_DOMAINS) + ")"
+
+    def _check_date_range(self, date_from: str, date_to: str) -> None:
+        """Check if a date range input is valid for a HogQL query"""
+
+        try:
+            DateRange(date_from=date_from, date_to=date_to)
+            logger.debug(f"Date range {date_from} and {date_to} is valid")
+        except ValidationError as e:
+            logger.error(f"Error validating date range: {date_from} and {date_to}: {e}")
+            raise
 
     def _make_request(
         self, endpoint: str = "query/", json_data: dict[str, Any] | None = None
@@ -61,25 +99,57 @@ class PostHogSession:
         data = self._make_request(json_data=payload)
         return data.get("results", [])
 
-    def count_unique_users(
+    def calculate_percentage_of_users_who_search(
         self,
-        date_from: str = "now() - interval 6 day",
-        date_to: str = "now()",
-    ) -> Count:
+        date_from: str,
+        date_to: str,
+    ) -> Percentage:
         """
-        Placeholder method: Count unique users in the time period, inclusive (default is last 7 days from current date)
+        Calculate the percentage of users who searched (NOT within a document) in the time period, inclusive of start and end dates.
 
-        :param date_from: start of time period, usually a date as string in format YYYY-MM-DD
-        :param date_to: end of time period, usually a date as string in format YYYY-MM-DD
-        :return: Count of unique users as a NonNegativeInt in the time period
+        What's a search?
+        A search is any filtered/unfiltered view of the results.  In that context, we consider search terms, page numbers, and regular topic/geography/etc filters are filters.  Thus, all of the following are valid search events:
 
+        - hitting the SERP (search engine results page) with some search terms e.g., "https://app.climatepolicyradar.org/search?q=kenya"
+        - going to the second page of results (this triggers URL change and API request)
+        - running a 'search' with no search terms, only filters
+        - running a 'search' with no search terms, no filters
+        - landing on the search page as the first pageview of a session
+
+        what's not a search
+        - searching within a document
+            - this is a search, but it's not the sort of search that most people are asking questions about.  It should be its own event, counted differently.
+        -clicking on a document from the search results (this triggers an API request)
+            - this is an event which appears in the user's search journey, but it's not a search.
+
+        "The total users" include only those with a pageview where the `consent` boolean is set.
+
+        See https://www.notion.so/climatepolicyradar/What-counts-as-a-search-2e89109609a48020b247fb9f19fac1de
+
+
+        :param date_from: start of time period (inclusive), a date as string in format YYYY-MM-DD
+        :param date_to: end of time period (exclusive), a date as string in format YYYY-MM-DD
+        :return: Percentage of users who searched in the time period as a float
         """
+
+        self._check_date_range(date_from, date_to)
         query = f"""
-            SELECT COUNT(DISTINCT properties.distinct_id) FROM events
-            WHERE timestamp >= {date_from}
-            AND timestamp <= {date_to}
+            SELECT 
+                count(Distinct(
+                    if(
+                        properties.$current_url LIKE '%/search%',
+                        distinct_id,
+                        NULL
+                    )
+                )) / count(Distinct(distinct_id)) * 100.0 AS search_percentage
+            FROM events
+            WHERE timestamp >= '{date_from} 00:00:00'
+                AND timestamp <= '{date_to} 23:59:59'
+                AND properties.consent IS NOT NULL
+                AND event = '$pageview'
+                AND properties.$host IN {self.cpr_domains}
         """
         results = self.execute_query(query)
         if not results:
             raise ValueError("PostHog query returned no results unexpectedly")
-        return Count(results[0][0])
+        return Percentage(results[0][0])
