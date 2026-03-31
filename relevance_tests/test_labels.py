698b0759a489a74a2c77cc731b13c0d6563f4e69
Kalyan Dutia <20212179+kdutia@users.noreply.github.com>
2026-03-31 12:29:18 +0100

    fix 'energy policy act us' test case
    - use stemmed form of policy
    - k=10 to account for number of relevant labels

--- relevance_tests/test_labels.py
+++ relevance_tests/test_labels.py
@@ -54,11 +54,13 @@ test_cases = [
         search_terms="energy policy act us",
         characteristics_test=lambda label: (
             "energy" in label.value.lower()
-            or "policy" in label.value.lower()
+            or "polic" in label.value.lower()
+            or "act" in label.value.lower()
             or "usa" in label.value.lower()
         ),
         all_or_any="all",
         description="search for energy policy act us should return relevant labels",
+        k=10,
     ),
     RecallTestCase[Label](
         category="document name",
