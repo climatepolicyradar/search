db1790fbc648830eea6a55e9dd76a44f61691093
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2026-04-08 17:48:54 +0100

    Hook otel up to search api (#231)
    * Merge OTEL instrumentors & main
    
    * Split code into separate files for maintainability
    
    * Create service-manifest.json
    
    * Setup OTel for search-api

--- api/dev.py
+++ api/dev.py
@@ -1,8 +1,15 @@
+import os
 from contextlib import asynccontextmanager
+from pathlib import Path
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from api.observability.src.api import (
+    FastAPITelemetry,
+    ServiceManifest,
+    TelemetryConfig,
+)
 from api.routers import router
 from search.log import get_logger
 
@@ -16,6 +23,30 @@ async def lifespan(_app: FastAPI):
     # Shutdown: Cleanup (if needed)
 
 
+# Configure Open Telemetry.
+ENV = os.getenv("ENV", "staging")
+os.environ["OTEL_PYTHON_LOG_CORRELATION"] = "True"
+_api_dir = Path(__file__).parent
+try:
+    otel_config = TelemetryConfig.from_service_manifest(
+        ServiceManifest.from_file(str(_api_dir / "service-manifest.json")),
+        ENV,
+        "0.1.0",
+    )
+except Exception as _:
+    logger.exception("Failed to load service manifest, using defaults")
+    otel_config = TelemetryConfig(
+        service_name="search-api",
+        namespace_name="data-querying",
+        service_version="0.0.0",
+        environment=ENV,
+    )
+
+telemetry = FastAPITelemetry(otel_config)
+tracer = telemetry.get_tracer()
+
+
+logger.debug("🚀 Starting FastAPI application")
 app = FastAPI(
     title="Climate Policy Radar Search API",
     description="API for searching climate policy documents, passages, and labels",
@@ -32,8 +63,11 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 app.include_router(router)
+
+
+telemetry.instrument_fastapi(app)
+telemetry.setup_exception_hook()
 
 
 # We use both routers to make sure we can have /search available publicly
