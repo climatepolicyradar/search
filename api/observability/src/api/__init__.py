50152c143fb2af9f10fd055d709fdfef42468f80
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2026-04-08 18:03:41 +0100

    Update __init__.py

--- api/observability/src/api/__init__.py
+++ api/observability/src/api/__init__.py
@@ -2,7 +2,6 @@ from .base_telemetry import BaseTelemetry
 from .fastapi_telemetry import FastAPITelemetry
 from .metrics import MetricsService
 from .service_manifest import ServiceManifest
-from .sqlalchemy_telemetry import SQLAlchemyTelemetry
 from .telemetry_config import TelemetryConfig
 from .telemetry_utils import convert_to_loggable_string, observe
 
@@ -11,7 +10,6 @@ __all__ = [
     "FastAPITelemetry",
     "MetricsService",
     "ServiceManifest",
-    "SQLAlchemyTelemetry",
     "TelemetryConfig",
     "convert_to_loggable_string",
     "observe",
