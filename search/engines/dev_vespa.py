527f2ce0691dd43bb3fcb2211f8735fa84ff9f26
Harrison Pim <hello@harrisonpim.com>
2026-04-16 16:52:54 +0100

    Use semantic rules to enable multi-word synonyms (#238)

--- search/engines/dev_vespa.py
+++ search/engines/dev_vespa.py
@@ -648,6 +648,7 @@ class DevVespaLabelSearchEngine(SearchEngine[Label]):
             "timeout": "5s",
             "model.language": "en",
             "ranking.profile": "nativerank",
+            "rules.rulebase": "labels",
         }
 
         try:
