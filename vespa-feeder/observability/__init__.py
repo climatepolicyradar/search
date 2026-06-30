7a1b262fac67785fe29de827ae7dffa8b8dd52b3
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2026-06-30 12:14:31 +0100

    Fix metrics propagation

--- vespa-feeder/observability/__init__.py
+++ vespa-feeder/observability/__init__.py
@@ -1,5 +1,6 @@
 from .base_telemetry import BaseTelemetry
 from .metrics import MetricsService
+from .prefect_telemetry import PrefectTelemetry, get_logger
 from .service_manifest import ServiceManifest
 from .telemetry_config import TelemetryConfig
 from .telemetry_utils import convert_to_loggable_string, observe
@@ -7,8 +8,10 @@ from .telemetry_utils import convert_to_loggable_string, observe
 __all__ = [
     "BaseTelemetry",
     "MetricsService",
+    "PrefectTelemetry",
     "ServiceManifest",
     "TelemetryConfig",
     "convert_to_loggable_string",
+    "get_logger",
     "observe",
 ]
