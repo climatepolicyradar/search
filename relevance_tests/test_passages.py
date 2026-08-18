9c0c9ef58f76ff465b60e2ec5deef24ac033c443
juliesaigusaCPR <julie.saigusa@climatepolicyradar.org>
2026-08-18 09:18:42 +0100

    add query rewrite rule for evs and loosen relevance test (#457)
    * add query rewrite rule for evs and loosen relevance test
    
    * add e2e test for passage query rewrites
    
    * add e2e test for passage query rewrites
    
    * add e2e test for passage query rewrites
    
    * fix stopword bug for gga
    
    * remove stopword from gga rewrite rule
    
    * add new relevance and revert gga rewrite change to show e2e test
    
    * change gga rule to exclude on so e2e test passes
    
    * update comment on test

--- relevance_tests/test_passages.py
+++ relevance_tests/test_passages.py
@@ -157,6 +157,19 @@ test_cases = [
         all_or_any="all",
         assert_results=True,
     ),
+        FieldCharacteristicsTestCase[Passage](
+        category="acronym-gga",
+        search_terms="gga",
+        characteristics_test=lambda passage: all_words_in_string(
+            ["global", "goal", "adaptation"], passage.text
+        )
+        and "gga" not in passage.text.lower(),
+        description="Acronyms: search for GGA should include global goal on adaptation but there should be some results without gga.",
+        k=700,
+        all_or_any="any",
+        assert_results=True,
+    ),
+
     FieldCharacteristicsTestCase[Passage](
         category="shortened phrase",
         # Anne observed a user do this and be very confused
@@ -196,7 +209,6 @@ test_cases = [
         search_terms="EVs",
         characteristics_test=(
             lambda passage: "electric car" in passage.text.lower()
-            and not any_words_in_string(["ev", "evs"], passage.text)
         ),
         description="Results for closely-related phrases (EVs -> electric car) should be found, even if the search phrase itself is not mentioned in the same paragraph",
         k=100,
